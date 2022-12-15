b53f1b8719127141229b55f1c57b657e5809032d
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2022-12-16 01:04:10 +0600

    Create Staircase.cpp

---
+++ Staircase.cpp
@@ -0,0 +1,14 @@
+void staircase(int n) {
+        for(int i=1; i<=n; i++)
+    {
+        for(int k=1; k<=n-i; k++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
